c57abe294cc7f21cbec234682c2e8401eb61235e
Daniel Tabion <dctabion@gmail.com>
2015-12-08 17:11:40 -0600

    fixed loading user_car object with params from search form

--- controllers/SearchController.rb
+++ controllers/SearchController.rb
@@ -9,19 +9,18 @@ post '/' do
   p '--------------posted search params!!!'
 
   user_car = {
-  "model" => params[model],
-  "inventory_number" => "6834053",
+  "make" => params[:make],
+  "model" => 'asdf',
   "style" => "4D",
   "color" => "BLK",
-  "towed_to_address" => "701 N. Sacramento",
-  "state" => "IL",
-  "plate" => "E540000**",
-  "tow_facility_phone" => "(773) 265-7605",
-  "tow_date" => "2015-12-08T00:00:00",
-  "make" => "BMW"
+  "state" => params[:state],
+  "plate" => params[:plate],
   }
 
+  p user_car
+
   @possible_matched_cars = search_for_matches(user_car)
+
 
   erb :index
 end
